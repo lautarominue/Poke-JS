30ceb3ed06f1cb5380a4da69f1d800dfb03220f7
Lautaro Minue <minue.lautaro7@gmail.com>
2023-03-31 03:56:11 -0300

    update 2

--- public/javascript/interaction/interaction.npc.js
+++ public/javascript/interaction/interaction.npc.js
@@ -1,8 +1,9 @@
 import { npcDetected } from "../function/index.js"
 import { printDialogDefault } from "../dialog/index.js"
+import { LOCALHOST } from "../utils/index.js"
 const interactionNpc = () => {
     const { id } = npcDetected
-    fetch(`http://localhost:3040/api/npc/code/${id}`)
+    fetch(`${LOCALHOST}/api/npc/code/${id}`)
         .then((response) => response.json())
         .then((data) => {
             printDialogDefault(data)
